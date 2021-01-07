79e795c2ffd79a896c482f98a0001360533f10de
Tyler Durrett <hi@tylerdurrett.com>
2021-01-07 17:55:05 -0600

    Add more controls to the Stack component

--- components/Stack/Stack.js
+++ components/Stack/Stack.js
@@ -3,6 +3,9 @@ import cn from 'classnames'
 
 export default function Stack({
   direction = 'column',
+  align = 'center',
+  justify = 'center',
+  fullWidth = true,
   children,
   ...rest
 }) {
@@ -12,6 +15,17 @@ export default function Stack({
         styles.stack, {
           [styles.column]: direction == 'column',
           [styles.row]: direction == 'row',
+          [styles.fullWidth]: fullWidth == true,
+          [styles.alignCenter]: align == 'center',
+          [styles.alignStretch]: align == 'stretch',
+          [styles.alignStart]: align == 'start',
+          [styles.alignEnd]: align == 'end',
+          [styles.justifyStart]: justify == 'start',
+          [styles.justifyEnd]: justify == 'end',
+          [styles.justifyCenter]: justify == 'center',
+          [styles.justifyAround]: justify == 'around',
+          [styles.justifyBetween]: justify == 'between',
+          [styles.justifyEven]: justify == 'even',
         }
       )}
       {...rest}
