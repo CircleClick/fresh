c85fd4de502f00f7e4e9057a037e9f99b8cfb0c8
Tyler Durrett <hi@tylerdurrett.com>
2021-01-08 11:23:46 -0600

    Add html lang parameter

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,16 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
+  /* Set a viewport height variable to avoid ugly 100vh issues on mobile */
   const {height} = useWindowDimensions();
   useEffect(() => {
     document.documentElement.style.setProperty('--viewportHeight', `${height}px`);
   }, [height]);
+
+  // Set the HTML lang to pass Lighthouse audits
+  useEffect(() => {
+    document.documentElement.lang = 'en-us';
+  }, []);
 
   return <Component {...pageProps} />
 }
