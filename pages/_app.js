656a7a04847d052d9ffbdb6026d3ac21c8746e57
Tyler Durrett <hi@tylerdurrett.com>
2021-01-07 18:46:46 -0600

    Fix the viewport height

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
+import {useEffect} from 'react'
+import useWindowDimensions from '../hooks/useWindowDimensions'
 import '../styles/variables.css'
 import '../styles/reset.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const {height} = useWindowDimensions();
+  useEffect(() => {
+    document.documentElement.style.setProperty('--viewportHeight', `${height}px`);
+  }, [height]);
+
   return <Component {...pageProps} />
 }
 
